cb421ce80ba265928ae6d794b38380dfa734779c
Leonardo Motta Maia <>
2019-05-06 19:35:02 -0300

    Adding the definition of the Erikson paper.

--- 1dDiffusionEq.py
+++ 1dDiffusionEq.py
@@ -1,127 +1,134 @@
 #!/usr/bin/python
 
-# Solves the linear advection equation monitoring the stability properties 
-# of the scheme. If it goes unstable, show the solution and the eigenvalues.
+# Solves the diffusion equation using a simple centered scheme.
+# While the iterations proceed, compute the eigenvalues of the 
+# D_RHS/D_U Jacobian in order to understand the spatial discre
+# tization stability.
 
 import numpy as np
 import sys
 from scipy.sparse import diags
 import matplotlib.pyplot as plt
+from autograd import grad, jacobian
 
 # Stable setup.
 
-nx = 200
+nx = 100
 dx = 2 / (nx-1)
 nt = 20
 nu = 0.1
-sigma = 0.2
-dt = sigma * dx**2 / nu
+dt = 0.001
 
 # Creates diagonal matrix based on diagonals.
 
 def create_diagonal(left,main,right,size):
    return diags([left, main, right], [-1, 0, 1], shape=(size, size)).toarray()
 
-# Creates diagonal matrix based on diagonals.
-
-def create_vdiag(left,main,right,size):
-
-    A = np.zeros(shape=(size,size))
-
-    np.fill_diagonal(A[1:], left)
-    np.fill_diagonal(A[:,:], main)
-    np.fill_diagonal(A[:,1:], right)
-
-    return A
+# Residue function of a simple centered three point scheme.
 
 def frhs(um1,uo1,up1,dx,nu):
     return nu*( (up1 - 2.0 * uo1 + um1)/dx**2.0 )
 
-# Create initial condition.
+# Main function.
 
-u = np.ones(nx)
+def main():
 
-# Apply initial condition.
+    # Create initial condition.
 
-u[int(.5 / dx):int(1 / dx + 1)] = 2
+    u = np.ones(nx)
 
-# Print initial condition.
+    # Apply the step condition.
 
-un = np.ones(nx)
+    u[int(.5 / dx):int(1 / dx + 1)] = 2
 
-rhs = np.zeros(nx)
+    # Print initial condition.
 
-# Iterate the solution and monitor the eigenvalues.
+    un = np.ones(nx)
 
-for n in range(nt):
+    # Prepare the residue vector.
 
-    # To solve numerically, copy the solution.
+    rhs = np.zeros(nx)
 
-    un = u.copy()
+    # Iterate the solution and monitor the eigenvalues.
 
-    # Separate the residue.
+    for n in range(nt):
 
-    for i in range(1, nx-1):
-        rhs[i] = frhs(un[i-1],un[i],un[i+1],dx,nu)
+        # Copy the solution of the explicit time marching scheme.
 
-    # March the residue.
+        un = u.copy()
 
-    for i in range(1, nx-1):
-        u[i] = un[i] + dt*rhs[i]
+        # Separate the residue.
 
-    # Computes the derivative of the residues with respect to the solution vector.
+        for i in range(1, nx-1):
+            rhs[i] = frhs(un[i-1],un[i],un[i+1],dx,nu)
 
-    eps = 0.001
+        # March the residue.
 
-    drhs_du = np.zeros((nx,nx))  # In order to take the eigenvalues, this shall be a matrix.
+        for i in range(1, nx-1):
+            u[i] = un[i] + dt*rhs[i]
 
-    for i in range(1,nx-1):
-        for j in range(1,nx-1):
-            drhs_du[i,j] = (frhs(un[i-1] + eps,un[i] + eps,un[i+1] + eps,dx,nu) - frhs(un[j-1],un[j],un[j+1],dx,nu))/eps
+        # Computes the derivative of the residues with respect to the solution vector.
 
-    # Build the hirsch matrix.
+        eps = 0.001
 
-    s_m = np.zeros((nx-1,nx-1))
+        drhs_du = np.zeros((nx-1,nx-1))  # In order to take the eigenvalues, this shall be a matrix.
 
-    # Fill the diagonals
+        d       = np.zeros((nx-1))       # The perturbation vector,
 
-    s_m = (nu/dx**2.0)*create_diagonal(1.0,-2.0,1.0,nx-1)
+        for i in range(1,nx-1):
+            d[i] = 1.0
+            for j in range(1,nx-1):
+                drhs_du[i,j] = (frhs(un[i-1] + eps*d[i],un[i] + eps*d[i],un[i+1] + eps*d[i],dx,nu) - frhs(un[j-1] + eps*d[i],un[j] + eps*d[i],un[j+1] + eps*d[i],dx,nu))/2.0*eps
+            d[i] = 0.0
 
-    # Solve the eigenvalues.
+        # Build the Hirsch matrix.
 
-    w1, v1 = np.linalg.eig(drhs_du)
-    w2, v2 = np.linalg.eig(s_m)
+        s_m = np.zeros((nx-1,nx-1))
 
-    # Prepare the plots.
+        # Fill the diagonals
 
-    real1 = np.zeros(nx)
-    imag1 = np.zeros(nx)
+        s_m = (nu/dx**2.0)*create_diagonal(1.0,-2.0,1.0,nx-1)
 
-    real1 = w1.real[:]
-    imag1 = w1.imag[:]
+        # Solve the eigenvalues.
 
-    real2 = np.zeros(nx)
-    imag2 = np.zeros(nx)
+        w1, v1 = np.linalg.eig(drhs_du)
+        w2, v2 = np.linalg.eig(s_m)
 
-    real2 = w2.real[:]
-    imag2 = w2.imag[:]
+        # Prepare the plots.
 
-    print ("\n")
-    print ("Maximun eigenvalues (Frechet): Real(eig): ", max(real1), " Imaginary: Imag(eig): ", max(imag1))
-    print ("Maximun eigenvalues (Hirsch ): Real(eig): ", max(real2), " Imaginary: Imag(eig): ", max(imag2))
+        real1 = np.zeros(nx)
+        imag1 = np.zeros(nx)
 
-    # plot the eigenvalues.
+        real1 = w1.real[:]
+        imag1 = w1.imag[:]
 
-    fig, ax = plt.subplots(3)
-    ax[0].plot(imag1, real1, 'ro')
-    ax[0].set(ylabel='Real(Eig)', xlabel='Imag(Eig)')
+        real2 = np.zeros(nx)
+        imag2 = np.zeros(nx)
 
-    ax[1].plot(imag2, real2, 'ro')
-    ax[1].set(ylabel='Real(Eig)', xlabel='Imag(Eig)')
+        real2 = w2.real[:]
+        imag2 = w2.imag[:]
 
-    ax[2].plot(np.linspace(0, 2, nx), u);
-    ax[2].set(xlabel='x', ylabel='u')
+        print ("\n")
+        print ("Minimun eigenvalues (Frechet): Real(eig): ", min(real1), " Imaginary: Imag(eig): ", min(imag1))
+        print ("Maximun eigenvalues (Frechet): Real(eig): ", max(real1), " Imaginary: Imag(eig): ", max(imag1))
+        print ("Minimun eigenvalues (Hirsch ): Real(eig): ", min(real2), " Imaginary: Imag(eig): ", min(imag2))
+        print ("Maximun eigenvalues (Hirsch ): Real(eig): ", max(real2), " Imaginary: Imag(eig): ", max(imag2))
 
-    plt.show(block=False)
-    plt.pause(0.5)
-    plt.close()
+        # plot the eigenvalues.
+
+        fig, ax = plt.subplots(3)
+        ax[0].plot(imag1, real1, 'ro')
+        ax[0].set(ylabel='Real(Eig)', xlabel='Imag(Eig)')
+
+        ax[1].plot(imag2, real2, 'ro')
+        ax[1].set(ylabel='Real(Eig)', xlabel='Imag(Eig)')
+
+        ax[2].plot(np.linspace(0, 2, nx), u);
+        ax[2].set(xlabel='x', ylabel='u')
+
+        plt.show(block=False)
+        plt.pause(0.5)
+        plt.close()
+
+if __name__ == '__main__':
+    main()
